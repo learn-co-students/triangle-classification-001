39350cc84a1738e3ab49016acd9e56a1d374147e
epang1 <epang1@gmail.com>
2017-04-10 16:38:59 -0400

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
+require 'pry'
 class Triangle
-  # write code here
+
+  attr_accessor :kind
+
+  def initialize(side1, side2, side3)
+
+    sides = [side1, side2, side3]
+    unique_sides = [side1.to_f, side2.to_f, side3.to_f].uniq
+
+
+    if unique_sides.any? {|x| x <= 0}
+      begin
+        raise TriangleError
+      end
+    end
+
+    sides.permutation(3).to_a.each do |a|
+      if a[0] + a[1] <= a[2]
+        begin
+          raise TriangleError
+        end
+      end
+    end
+
+    if unique_sides.count == 1
+      self.kind = :equilateral
+    elsif unique_sides.count == 2
+      self.kind = :isosceles
+    else
+      self.kind = :scalene
+    end
+
+  end
+
+end
+
+class TriangleError < StandardError
+  def message
+    "Must be >0"
+  end
 end
